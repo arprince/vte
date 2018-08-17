2bda5095fcc4e674c155ac3a7de913481e113df1
Egmont Koblinger <egmont@gmail.com>
2018-08-17 20:00:14 +0200

    BDSM and SPD escape sequences

--- src/modes.hh
+++ src/modes.hh
@@ -164,7 +164,8 @@ public:
 #undef MODE
 #undef MODE_FIXED
 
-        constexpr ECMA() : Self{eSRM} { }
+        constexpr ECMA() : Self{eBDSM,
+                                eSRM} { }
 
 }; // class ECMA
 
